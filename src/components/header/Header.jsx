c8ec08653780b8026dca821b9d5d17fe19d04bfa
michel_hightao <michel@hightao-mg.com>
2021-11-14 18:10:50 +0300

    initial commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 import './Header.scss';
 
-import fincorpLogo from '../../assets/img/logo.png';
-
 const Header = () => (
   <div className="comp-header">
-    <img src={fincorpLogo} alt="logo" />
+    <img src="https://firebasestorage.googleapis.com/v0/b/sayna-test-99ab6.appspot.com/o/logo.png?alt=media&token=ab692473-ec9c-40f2-9fda-3240451f7576" alt="logo" />
     <span>For You & Family</span>
     <span>For Business</span>
   </div>
