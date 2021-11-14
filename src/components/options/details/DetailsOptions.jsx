ea0bb2db23b4482f97eed1eb5605f8a950238932
michel_hightao <michel@hightao-mg.com>
2021-11-14 18:21:45 +0300

    initial commit

--- src/components/options/details/DetailsOptions.jsx
+++ src/components/options/details/DetailsOptions.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import './DetailsOptions.scss';
 
-import tick from '../../../assets/img/tick.png';
-
 const DetailsOptions = (props) => {
   const { title, children } = props;
 
   return (
     <div className="comp-details-options">
       <div className="comp-details-options__icon">
-        <img src={tick} alt="tick" className="comp-details-options__icon-img" />
+        <img src="https://firebasestorage.googleapis.com/v0/b/sayna-test-99ab6.appspot.com/o/tick.png?alt=media&token=436b9769-102d-408d-80b9-90dfd1b8e29c" alt="tick" className="comp-details-options__icon-img" />
       </div>
       <div className="comp-details-options__text">
         <span className="comp-details-options__text-title">
